722c2b84f9fbd61c0c59eff8a94666b2c512fa74
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-07-01 11:05:44 +0530

    Add files via upload

---
+++ Run_time_polymorphism_With_data_member.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using  namespace std;
+class Animal
+{
+public:
+
+   string color="Black";
+};
+class Dog:public Animal
+{
+public:
+    string color="Grey";
+};
+int main()
+{
+    Animal d =Dog();
+    cout<<d.color;
+    return 0;
+}
+
