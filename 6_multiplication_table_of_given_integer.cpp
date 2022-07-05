95198baacede6ea523fcc2736bfd3060b65601c1
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-07-05 16:47:54 +0530

    Add files via upload

---
+++ 6_multiplication_table_of_given_integer.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i_num=15,i_multi=0,i=0;
+
+    for(i=1;i<=15;i++)
+    {
+        i_multi=i*i_num;
+        cout<<endl<<"15*"<<i<<"= "<<i_multi;
+    }
+    return 0;
+}
