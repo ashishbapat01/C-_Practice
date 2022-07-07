7d144bb5a83d24fdc48dc43f93688d22cf0b831d
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-07-07 12:07:14 +0530

    Add files via upload

---
+++ 11_pattern_like_right_angle_triangle_with_a_number_which_will_reapeat_a_number_in_row.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i_num=0;
+
+    cout<<"Enter the number:- ";
+    cin>>i_num;
+
+    for(int i=1;i<=i_num;i++)
+    {
+        for(int j=1;j<=i_num;j++)
+        {
+            if(i<j)
+            {
+                cout<<"";
+            }
+            else
+            {
+                cout<<" "<<i;
+            }
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
