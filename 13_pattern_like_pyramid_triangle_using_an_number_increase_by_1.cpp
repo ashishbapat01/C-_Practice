b482aa1d6c6d452ac2027e46769e5bda8c7411ad
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-07-10 21:32:46 +0530

    Add files via upload

---
+++ 13_pattern_like_pyramid_triangle_using_an_number_increase_by_1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i_num=0,k=0,p=1;
+
+    cout<<"Enter the number:- ";
+    cin>>i_num;
+
+    for(int i=1;i<=i_num;i++)
+    {
+        for(int j=1;j<=i_num-i;j++)
+           {
+                cout<<" " ;
+           }
+        for(int k=1;k<=i;k++)
+          {
+                 cout<<" "<<p++;
+          }
+          cout<<endl;
+       }
+
+    return 0;
+}
+
