6621e0d6d98408b94244fd79fe39e5462fd9bab0
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-07-31 11:57:33 +0530

    Add files via upload

---
+++ loops/5_Display_cube_of_number_upto_given_an_integer.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i_num=5,i_cube=0,i=0;
+
+    for(i=1;i<=5;i++)
+    {
+        i_cube=i*i*i;
+        cout<<endl<<"Number is :"<<i<< " and cube of the "<<i<< " is :- "<<i_cube;
+    }
+   return 0;
+}
