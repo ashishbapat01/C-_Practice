d20bce3da8a0fe7c344318c8ad74aa6636dba013
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-08-02 18:02:25 +0530

    Add files via upload

---
+++ loops/10_pattern_like_right_angle_triangle_using_an_number.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i_num=0;
+
+    cout<<"Enter the number:- ";
+    cin>>i_num;
+
+    for(int i=1;i<=i_num;i++)
+    {
+        for(int j=1;j<=i_num;j++)
+        {
+            if(i<j)
+            {
+                cout<<"";
+            }
+            else
+            {
+                cout<<" "<<j;
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
