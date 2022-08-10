d3072b37e2b4f37305424aa6cfc23aa9c41d67cc
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-08-10 10:47:36 +0530

    Add files via upload

---
+++ loops/17_pattern_with_pyramid_with_a_number_which_will_repeate_the_number_in_the_same_row.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i_num=0,k=0;
+
+    cout<<"Enter the number:- ";
+    cin>>i_num;
+
+    for(int i=1;i<=i_num;i++)
+    {
+        for(int j=1;j<=i_num-i;j++)
+           {
+                cout<<" " ;
+           }
+        for(int k=1;k<=i;k++)
+          {
+                 cout<<" "<<i;
+          }
+          cout<<endl;
+       }
+
+    return 0;
+}
+
+
