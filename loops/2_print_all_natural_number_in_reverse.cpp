8338a0062c28f17803cdfb33340e8e673b1946e9
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-08-23 16:16:44 +0530

    Add files via upload

---
+++ loops/2_print_all_natural_number_in_reverse.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i_num=0, i=0;
+
+    cout<<"Enter Any Number:- ";
+    cin>>i_num;
+
+    for(i=i_num;i>=1;i--)
+    {
+        cout<< i ;
+    }
+    return 0;
+}
+
