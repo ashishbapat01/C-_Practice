3ea256c5a8c5257296753e9dc497ca38911bccd2
Ashish Bapat <91781811+ashishbapat01@users.noreply.github.com>
2022-09-03 15:55:34 +0530

    Add files via upload

---
+++ loops/8_pattern.cpp
@@ -0,0 +1,29 @@
+ #include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i_num=0;
+
+    cout<<"Enter Any Number:- ";
+    cin>>i_num;
+
+    for(int i=1;i<=i_num;i++)
+    {
+      for(int j=1;j<=i_num;j++)
+      {
+          if(i>j)
+          {
+              cout<<"";
+          }
+          else
+          {
+             cout<<"* ";
+          }
+      }
+      cout<<endl;
+    }
+
+   return 0;
+}
